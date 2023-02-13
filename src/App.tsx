0631f2a7a16422c57a7e4c424404a130b1b3514d
Marc Girbau <marc.girbau94@gmail.com>
2023-02-13 22:25:26 +0100

    Create TodoList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import TodosList from "./components/TodosList/TodosList";
 
 const App = (): JSX.Element => {
-  return <div>Hola</div>;
+  return (
+    <div>
+      Hola
+      <TodosList />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/TodosList/TodosList.tsx
@@ -0,0 +1,10 @@
+const TodosList = (): JSX.Element => {
+  return (
+    <ul>
+      <li>Buy groceries</li>
+      <li>Buy groceries</li>
+    </ul>
+  );
+};
+
+export default TodosList;
